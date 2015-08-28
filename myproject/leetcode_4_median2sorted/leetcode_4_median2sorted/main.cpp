f44423c46182864768ac35425ee39c1a8dbd66eb
mathieuwang <mathieuwang@gmail.com>
2015-08-28 18:32:36 -0400

    +) latched problem 4 partially

---
+++ myproject/leetcode_4_median2sorted/leetcode_4_median2sorted/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  leetcode_4_median2sorted
+//
+//  Created by mathieuwang on 15/8/28.
+//  Copyright (c) 2015年 mathieuwang. All rights reserved.
+//
+
+#include <iostream>
+#include "mediantwosortedarr.cpp"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ myproject/leetcode_4_median2sorted/leetcode_4_median2sorted/mediantwosortedarr.cpp
@@ -0,0 +1,19 @@
+//
+//  mediantwosortedarr.cpp
+//  leetcode_4_median2sorted
+//
+//  Created by mathieuwang on 15/8/28.
+//  Copyright (c) 2015年 mathieuwang. All rights reserved.
+//
+
+#include <stdio.h>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        
+    }
+};
